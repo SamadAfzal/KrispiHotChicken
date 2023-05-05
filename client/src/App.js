adcd907e143da3b7873f97c0b352fd164c1d40e1
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2023-05-04 20:23:32 -0700

    footer + fire begin

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Fire from './components/fire';
 import "./styles/css/styles.css"
 
 function App() {
@@ -7,9 +9,13 @@ function App() {
     <div>
       <Navbar />
       <h1>Krispi Hot Chicken</h1>
+      <canvas>
+         <a href="/Order">Order</a>
+      </canvas>
+      <Fire />
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,11 @@ import React from 'react';
 function Footer() {
     return(
         <div>
-            FOOTER
+            <h3>Store Location</h3>
+            <p>1234 Main St.</p>
+            <p>City, State 12345</p>
+            <p>Phone: 123-456-7890</p>
+            <h3>Store Hours: 10:30 A.M. - 12 A.M. </h3>
         </div>
     );
 };
