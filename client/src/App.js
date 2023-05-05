5a73fd394d055e74cbfb50d9a458cfed31ff695b
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2023-05-04 20:25:45 -0700

    commit#

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Fire from './components/fire';
 import "./styles/css/styles.css"
 
 function App() {
   return (
     <div>
       <Navbar />
-      <h1>Krispi Hot Chicken</h1>
-      <canvas>
-         <a href="/Order">Order</a>
-      </canvas>
-      <Fire />
-      <Footer />
     </div>
   );
 }
