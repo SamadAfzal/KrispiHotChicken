5cd22e43073bc13c2bf0375d4077d5c2bab2407f
Samad <samadafzal1@g.ucla.edu>
2023-05-04 19:09:07 -0700

    home page code along with button

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,8 +4,18 @@ import React from 'react';
 
 function Home() {
   return (
-    <div></div>
+    <div className="home-container">
+      <Navbar />
+      <div className="video-container">
+        {/*video code here*/}
+        <video autoPlay muted loop>
+          <source src={video} type="video/mp4" />
+        </video>
+        <div className="overlay">
+          <button className="order-button">Order Now</button>
+        </div>
+      </div>
+      <Footer />
+    </div>
   );
 }
-
-export default Home;
